1bcb07c7da142916848f5d2da6bb3c174d41e557
abhishekjoshi77 <94050150+abhishekjoshi77@users.noreply.github.com>
2025-05-11 16:49:48 +0530

    Update ThemeContext.jsx

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,1 +1,25 @@
 
+import { createContext, useContext, useEffect, useState } from "react";
+import { useColorMode } from "@chakra-ui/react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  useEffect(() => {
+    if (theme !== colorMode) {
+      toggleColorMode();
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme, colorMode, toggleColorMode]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useThemeContext = () => useContext(ThemeContext);
