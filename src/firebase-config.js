7b805bb8a6e8dd8bdcbcfde9d5818960ecb17aff
abhishekjoshi77 <94050150+abhishekjoshi77@users.noreply.github.com>
2025-05-11 16:55:43 +0530

    Create firebase-config.js

---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
